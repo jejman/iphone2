2a33accddb2efcca70e84172ad988e77fca4ad49
jejman <134877489+jejman@users.noreply.github.com>
2023-07-09 00:33:33 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('unlock-button').addEventListener('click', function() {
+    document.getElementById('lock-screen').style.display = 'none';
+    document.getElementById('home-screen').style.display = 'block';
+  });
+  
